987bce33ae3c87c3f7952192af4b51abafe68601
Stefan <stefan.d.yordanov@gmail.com>
2023-05-26 20:44:31 +0100

    Generate home and bio components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BioComponent } from './bio/bio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BioComponent
   ],
   imports: [
     BrowserModule
