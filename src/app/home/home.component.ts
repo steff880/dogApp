46ce2ed6ac5ef95358f5b7eca743852554c002f5
Stefan <stefan.d.yordanov@gmail.com>
2023-05-26 21:02:00 +0100

    Add funcitonality to home component to be able to retreive and store dog data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Dog } from '../dog';
+import { DogsService } from '../dogs.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  // Create an empty list that will have to have the same types and the interface
+  dogsList: Dog[] = [];
+  // inject the service to allow the use of its methods
+  dogsService: DogsService = inject(DogsService);
 
+  // Since the constructor is the first to run we want to get all our data and get it ready for use
+  constructor() {
+    // Get all dogs and update the list. Since getAllDogs mehtod returns a Promise we need to use "then" to asyncronously get the data
+    this.dogsService.getAllDogs().then((dogsList: Dog[]) => {
+      this.dogsList = dogsList;
+    });
+  }
 }
